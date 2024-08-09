41a551b7b743082ddfd383347c56a66e7114faa0
YesseniaB <yessenia.baldoceda@gmail.com>
2024-08-08 20:46:58 -0500

    first commit

---
+++ src/test/java/Formulario.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Formulario {
+    public static void main(String[] args) {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("star-maximized");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://demoqa.com/");
+        driver.quit();
+
+    }
+}
